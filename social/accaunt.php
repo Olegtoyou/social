3341989cf7fd65c4e6ca54a7a8efa6bbab8b959f
Olegtoyou <122714052+Olegtoyou@users.noreply.github.com>
2024-04-05 19:36:04 +0500

    Add files via upload

---
+++ social/accaunt.php
@@ -0,0 +1,2 @@
+<h3>Панель управления</h3>
+<button onclick="document.location='editer_profile.php'" id="edit-profile">Редактировать Профиль</button>
---
+++ social/js/profile.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    const accountCreationDate = document.getElementById("accaunt-creation-date");
+    const userEmail = document.getElementById("user-email");
+    const userInfo = document.getElementById("user-info");
+    // Функция для загрузки информации о пользователе
+    async function loadProfileInfo() {
+        try {
+            const response = await fetch('getProfileInfo.php', {
+                method: 'POST',
+            });
+            const data = await response.text();
+            // Разбиваем строку на отдельные значения
+            const [email, created, info] = data.split('|');
+            // Устанавливаем значения полей профиля
+            userEmail.innerText = email;
+            accountCreationDate.innerText = created;
+            userInfo.innerText = info;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+    // Загружаем информацию о пользователе при загрузке страницы
+    loadProfileInfo();
+});
