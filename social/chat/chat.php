adc6df4773e9066cdb22a8f331b7b19e44d87a58
Olegtoyou <122714052+Olegtoyou@users.noreply.github.com>
2024-04-07 19:28:29 +0300

    Add files via upload

---
+++ social/chat/chat.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="device-width, initial-scale=1.0">
+<title>Чат</title>
+<link rel="stylesheet" href="../css/menu.css">
+<link rel="stylesheet" href="../css/chat-styles.css">
+</head>
+<body>
+<?php
+include("../menu.php");
+?>
+<div id="chat-container">
+<div id="user-list">
+<h3>Пользователи</h3>
+<ul id="all-users">
+<!-- Здесь будут отображаться все пользователи -->
+<!-- Пример: <li class="selected">Пользователь 1</li> -->
+</ul>
+</div>
+<div id="chat-messages">
+<h3>Выберите пользователя из списка, для начала общения 🥰</h3>
+<!-- Здесь будут отображаться сообщения чата -->
+<?php
+if (isset($_SESSION['user_id'])) {
+ echo '<script>selectedUserId = ' . $_SESSION['user_id'] . ';</script>';
+include 'getMessages.php';
+ }
+?>
+</div>
+</div>
+<div id="message-input">
+<input type="text" id="message" contenteditable="true" placeholder="Введите ваше сообщение...">
+<div id="sticker-trigger" onclick="toggleStickerList()">😊</div>
+<div id="sticker-list" class="hidden">
+<div class="sticker" onclick="insertSticker('🙂')">🙂</div>
+<div class="sticker" onclick="insertSticker('😊')">😊</div>
+<div class="sticker" onclick="insertSticker('🌟')">🌟</div>
+<div class="sticker" onclick="insertSticker('🎉')">🎉</div>
+<div class="sticker" onclick="insertSticker('😶🌫')">😶🌫</div>
+<!-- Добавьте другие стикеры по аналогии -->
+</div>
+<button id="send-button" onclick="sendMessage()">Отправить</button>
+</div>
+<script src="../js/chat.js"></script>
+</body>
+</html>
--- social/menu.php
+++ social/menu.php
@@ -1,10 +1,21 @@
-<input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon" />
+<input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon"/>
 <label for="menu-icon"></label>
 <nav class="nav">
-    <ul class="pt-5">
-        <li><a href="index.php">Главная</a></li>
-        <li><a href="auth.php">Личный кабинет</a></li>
-        <li><a href="#">Чат</a></li>
-        <li><a href="#">Новости</a></li>
-    </ul>
+<ul class="pt-5">
+<li><a href="http://localhost/Social/">Главная</a></li>
+<!-- Добавляем PHP-код для определения ссылки на страницу "Личный кабинет" и «Чат»-->
+<?php
+if (isset($_SESSION['id'])) {
+ echo '<li><a href="http://localhost/Social/profile/accaunt.php">Личный кабинет</a></li>';
+ } else {
+ echo '<li><a href="http://localhost/Social/auth.php">Личный кабинет</a></li>';
+ }
+if (isset($_SESSION['id'])) {
+ echo '<li><a href="http://localhost/Social/chat/chat.php">Чат</a></li>';
+ } else {
+ echo '<li><a href="http://localhost/Social/auth.php">Чат</a></li>';
+ }
+?>
+<li><a href="#">Новости</a></li>
+</ul>
 </nav>
